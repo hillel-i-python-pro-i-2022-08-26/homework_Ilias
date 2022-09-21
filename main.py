b84fbf2aaf4b8a3c3e8a12fa619c8de92bd49a1c
ilyas <ilyasutemisov@gmail.com>
2022-09-21 18:22:34 +0200

    Added files (requirments, pre-commit, flake8)

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-# homework ggwp lose
-# 1213
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "My Hometask"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
