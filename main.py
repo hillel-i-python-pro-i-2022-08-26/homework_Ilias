1ba09812c2eccfeb79ced38861d2fdc7357bc0e7
ilyas <ilyasutemisov@gmail.com>
2022-09-21 19:48:20 +0200

    Task 3 ( generate users ) add 2 functions with users and mail

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 from pathlib import Path
 
+from faker import Faker
 from flask import Flask
 
+fake = Faker()
 app = Flask(__name__)
 path_file = Path("file.txt")
+
+
+def users():
+    name = fake.first_name()
+    mail = f"{str(name.lower())}_@gmail.com"
+    yield f"{name}: {mail}"
+
+
+# route_users_generate_by_number__start
+@app.route("/generate-users/<int:quantity>")
+@app.route("/generate-users/")
+def generated_users(quantity: int = 100):
+    num = quantity
+    for i in range(num):
+        for name in users():
+            yield f"<p>{i + 1}. {name}</p>"
 
 
 @app.route("/")
