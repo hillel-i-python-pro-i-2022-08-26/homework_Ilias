fc74c654870350b4a29a44dcdadde4fca01b4fc1
ilyas <ilyasutemisov@gmail.com>
2022-09-21 18:39:25 +0200

    Added routes

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+from pathlib import Path
+
 from flask import Flask
 
 app = Flask(__name__)
+path_file = Path("file.txt")
 
 
 @app.route("/")
 def index():
     return "My Hometask"
+
+
+@app.route("/requirements")
+def file():
+    return path_file.read_text()
 
 
 if __name__ == "__main__":
