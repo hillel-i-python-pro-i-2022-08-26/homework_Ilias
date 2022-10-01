aa39bf1b22d7fa53217542b0b85703e759214805
ilyas <ilyasutemisov@gmail.com>
2022-10-01 19:58:33 +0200

    update data

--- application/services/db_tab.py
+++ application/services/db_tab.py
@@ -4,9 +4,11 @@ from application.services.dbconnection import DbConnection
 def table():
     with DbConnection() as connection:
         with connection:
-            connection.execute("""
+            connection.execute(
+                """
             CREATE TABLE IF NOT EXISTS phones(
             phoneID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
             contactName VARCHAR NOT NULL,
             phoneNumber INTEGER NOT NULL
-            )""")
+            )"""
+            )
--- application/settings.py
+++ application/settings.py
@@ -3,4 +3,4 @@ import pathlib
 path_file = pathlib.Path("file.txt")
 
 route = pathlib.Path(__file__).parents[1]
-db_route = route.joinpath('database', 'dbproject.sqlite')
+db_route = route.joinpath("database", "dbproject.sqlite")
